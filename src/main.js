3edcf52337a3113ca0798a02a66e4ecdd630df82
mquintero0118 <m.alejandro.quintero@gmail.com>
2022-11-29 21:39:19 -0600

    installed primevue

--- src/main.js
+++ src/main.js
@@ -1,6 +1,24 @@
+/* eslint-disable */
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import PrimeVue from 'primevue/config';
+import Button from 'primevue/button';
 
-createApp(App).use(store).use(router).mount('#app')
+
+import 'primevue/resources/themes/saga-blue/theme.css'       //theme
+import 'primevue/resources/primevue.min.css'               //core css
+import 'primeicons/primeicons.css'
+
+
+const app = createApp(App);
+
+app.use(PrimeVue)
+app.use(store)
+app.use(router)
+
+
+app.component('Button', Button);
+
+app.mount('#app')
